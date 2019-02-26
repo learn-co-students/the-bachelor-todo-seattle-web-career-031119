22dc86f7c20a5540e2eb80784cc5917b47265ad6
jyang81 <joe.yang.design@gmail.com>
2019-02-25 23:46:38 -0800

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data[season].each do |array|
+        if array["status"] == "Winner"
+        return array["name"].split.first
+        end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    data.each do |season,array|
+        array.each do |person|
+#            binding.pry
+            if person["occupation"] == occupation
+            return person["name"]
+            end
+        end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+    counter = 0
+    data.each do |season,array|
+        array.each do |person|
+        if person["hometown"] == hometown
+            counter += 1
+#            binding.pry
+        end
+        end
+    end
+    counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season,array|
+        array.each do |person|
+            if person["hometown"] == hometown
+                return person["occupation"]
+            end
+        end
+    end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+    ages = []
+    data[season].each do |array|
+        ages << array["age"].to_i
+#        binding.pry
+    end
+    avg = ages.inject(:+)/ages.size.to_f
+#    binding.pry
+    return avg.round
 end
